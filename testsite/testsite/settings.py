c48898af368fd941a34abf77b82507c91d47fea7
Victor Godoy Poluceno <victorpoluceno@gmail.com>
2013-02-22 14:24:09 -0300

    Initiall restfull api implementation

--- testsite/testsite/settings.py
+++ testsite/testsite/settings.py
@@ -120,7 +120,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
-    'polls'
+    'polls',
+    'tastypie'
 )
 
 # A sample logging configuration. The only tangible logging
