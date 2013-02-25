94851bd61219771b59603c4593a9650158bc373d
Renne Rocha <renne@rennerocha.com>
2013-02-24 22:26:45 -0300

    View para receber votações.

--- poll_project/poll/views.py
+++ poll_project/poll/views.py
@@ -1,8 +1,26 @@
 # -*- coding: utf-8 -*-
+from django.shortcuts import redirect, render_to_response
+from django.template import RequestContext
 from django.views.generic import ListView
-from .models import Poll
+
+from .forms import PollVoteForm
+from .models import Choice, Poll
 
 
 class PollList(ListView):
     model = Poll
 
+
+def vote(request, poll_id):
+
+    poll = Poll.objects.get(id=poll_id)
+
+    if request.method == 'POST':
+        form = PollVoteForm(data=request.POST, poll_id=poll_id)
+        if form.is_valid():
+            return redirect('/polls')
+    else:
+        form = PollVoteForm(poll_id=poll_id)
+
+    return render_to_response('vote.html', {'form': form, 'poll': poll}, 
+        context_instance=RequestContext(request))
--- poll_project/poll_project/urls.py
+++ poll_project/poll_project/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 
-from poll.views import PollList
+from poll.views import PollList#, PollVoteView
 
 
 admin.autodiscover()
@@ -11,5 +11,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     (r'^api/', include('poll.urls')),
     url(r'^polls/$', PollList.as_view()),
+    url(r'^polls/vote/(?P<poll_id>[^/]+)/$', 'poll.views.vote', name='vote'),
     url(r'^admin/', include(admin.site.urls)),
 )
